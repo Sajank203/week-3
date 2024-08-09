b7f32c41671dcd36e5297d9cb18765cd1f151ab0
Sajank203 <SAJANPREET1712003@ICLOUD.COM>
2024-08-09 13:33:10 +1200

    Add files via upload
    square area

---
+++ squareArea.py
@@ -0,0 +1,9 @@
+# Prompt the user to input the radius of the circle
+x= float(input("input the square sides : "))
+
+
+# calculate the area of the circle using the formula: area = π * r^2
+area = x**2
+
+# Display the result, including the radius and calculated area 
+print("The area of the square has the side x= " + str(x)  +  " is: " + str(area))
