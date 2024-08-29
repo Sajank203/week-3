7765c172ec8920795c00ae0156ba3213531b329b
Sajank203 <SAJANPREET1712003@ICLOUD.COM>
2024-08-29 12:56:15 +1200

    Add files via upload
    task 1 week 6

---
+++ task 1.py
@@ -0,0 +1,10 @@
+class Person:
+def __init__(self, name, age):
+self.name = name
+self.age = age
+person1 = Person("Alice", 30)
+person2 = person("sajan", 21)
+person 3 = person("Lukas", 22)
+print(f"{person1.name} is {person1.age} years old.")
+print(f"{person2.name} is {person2.age} years old.")
+print(f"{person3.name} is {person3.age} years old.")
